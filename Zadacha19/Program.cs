7c257213173a04134b034ea028fee313e7d6d5ad
igorminash <iminash@inbox.ru>
2022-11-23 20:21:46 +0300

    Добавлено решение 19 задачи

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine()!;
+
+if (number.Length < 5 || number.Length > 5)
+{
+    Console.WriteLine("Введено неверное число");
+}
+    else 
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+    
+}
+
